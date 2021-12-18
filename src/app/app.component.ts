b508b3f0144847d615a13ce8a13ad52da6640a2f
Gerald Gbagbe <geraldelorm14@gmail.com>
2021-12-18 18:09:28 +0000

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{ pageTitle }}</h1>
+      <div>My First Component</div>
+    </div>
+  `,
+})
+export class AppComponent {
+  pageTitle: String = 'Acme Product Management';
+}
